1cafaa79c8ecaf2ef482bdab90430cc1addc48d4
Danil <dan-kull@mail.ru>
2025-12-07 13:37:29 +0300

    adaptive done

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../public/css/main.css';
+import '../public/css/responsive.css';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store, fetchOffers, checkAuth } from './store';
